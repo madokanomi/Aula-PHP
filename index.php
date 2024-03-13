7f80b2daa5adff7fe2a44a179d3249064db6cf2c
Felipe Cruz <josepereiratuamae@gmail.com>
2024-03-13 17:31:35 -0300

    Sale esteve aqui

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira aula de PHP</title>
+</head>
+<body>
+    <?php
+
+$nome = "Felipe limdo";
+$idade = 2;
+$altura = 2.22;
+$peso = 2.22;
+
+var_dump($nome);
+echo "<br>";
+var_dump($idade);
+echo "<br>";
+var_dump($altura);
+echo "<br>";
+var_dump($peso);
+echo "<br>";
+echo "Olá Mundo!";
+
+?>
+</body>
+</html>
